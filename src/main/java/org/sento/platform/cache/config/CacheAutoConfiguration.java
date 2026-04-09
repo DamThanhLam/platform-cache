ccf4dc16980a04c2fd79b3e9cd64166668c373c6
DamThanhLam <ldamthanh@gmail.com>
2026-04-09 08:41:49 +0700

    add autoconfigure

--- src/main/java/org/sento/platform/cache/config/CacheAutoConfiguration.java
+++ src/main/java/org/sento/platform/cache/config/CacheAutoConfiguration.java
@@ -1,9 +1,12 @@
 package org.sento.platform.cache.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 @ComponentScan(basePackages = "org.sento.platform.cache")
+@ConditionalOnClass(RedisConnectionFactory.class)
 public class CacheAutoConfiguration {
 }
